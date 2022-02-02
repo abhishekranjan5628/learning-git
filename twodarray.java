0b9ee5c821ecc106b75f7be8f905b96d40908a9d
abhishekranjan5628 <abhishekranjan5628@gmail.com>
2022-02-03 01:44:03 +0530

    print

--- twodarray.java
+++ twodarray.java
@@ -1,6 +1,7 @@
 class twodarray {
     public static void main (String[]args){
       int arr[][]=new int[2][2];
-      arr[0][1]
+      arr[0][1]=20;
+      System.out.println(arr[0][1]);
     }
 }
